8c4f2b94a9a0ddba075216a0cb0adfa1316b7ad4
IsaacMCorpas <7isaac20@gmail.com>
2018-05-25 21:08:36 +0200

    arreglos

--- src/main/java/com/soundclub/controllers/ResgistroServlet.java
+++ src/main/java/com/soundclub/controllers/ResgistroServlet.java
@@ -2,18 +2,27 @@ package com.soundclub.controllers;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-
+@WebServlet("/Registrar_usuario")
 public class ResgistroServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		HttpSession registro = request.getSession();
+		
+		if (registro.getAttribute("email")!=null && registro.getAttribute("password")!=null) {
+			response.sendRedirect("Registrar_usuario");
+		}else {
+			request.getRequestDispatcher("index.jsp").forward(request, response);	
+		}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
